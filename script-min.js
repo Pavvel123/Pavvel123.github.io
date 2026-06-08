8643a59be6b0f2dd1413af1c5b195b84f6f2198d
Paweł Kurpiewski <94716283+Pavvel123@users.noreply.github.com>
2026-06-08 14:44:52 +0200

    Update script-min.js

--- script-min.js
+++ script-min.js
@@ -2,16 +2,16 @@ const SEMESTER_START="2026-02-23",SEMESTER_WEEKS=22,C_W="#55a2eb",C_C="#ef569d",
 {id:"XBG",title:"Obowiązuje plan z piątku!",day:3,start:"07:30",end:"22:00",place:"",teacher:"",color:"#007413ff",weeks:[6]},//1.04
 
 {id:"TRP",title:"Technika radia programowalnego - zajęcia wprowadzające",color:C_L,day:1,weeks:[10],start:"09:15",end:"11:00",place:"NE 209",teacher:"dr inż. Andrzej Marczak"},
-{id:"TRP",title:"Technika radia programowalnego",color:C_L,day:1,weeks:[11,12,13,14,15,16],start:"09:15",end:"12:00",place:"EA 407",teacher:"dr inż. Andrzej Marczak"},
+{id:"TRP",title:"Technika radia programowalnego",color:C_L,day:1,weeks:[11,12,13,14,15],start:"09:15",end:"12:00",place:"EA 407",teacher:"dr inż. Andrzej Marczak"},
 //{id:"BW",title:"Bazy wiedzy",color:C_W,day:1,weeks:[3,5,6,8,9],start:"13:15",end:"15:00",place:"NE AUD2",teacher:"dr inż. Wojciech Waloszek"},
 //{id:"ED",title:"Eksploracja danych",color:C_W,day:1,weeks:[3,5,6,8,9],start:"15:15",end:"17:00",place:"NE AUD2",teacher:"dr inż. Wojciech Waloszek"},
 {id:"ZFI",title:"Zarządzanie firmą informatyczną",color:C_P,day:1,weeks:[10,13,14,15,16],start:"15:15",end:"17:00",place:"NE 237",teacher:"dr inż. Paweł Jakóbczyk"},
-{id:"WZR",title:"Wirtualne zespoły robocze",color:C_L,day:1,weeks:[1,3,5,8,10,12,14,16],start:"17:15",end:"19:00",place:"EA 401",teacher:"dr inż. Jerzy Dembski"},
+{id:"WZR",title:"Wirtualne zespoły robocze",color:C_L,day:1,weeks:[1,3,5,8,10,12,14],start:"17:15",end:"19:00",place:"EA 401",teacher:"dr inż. Jerzy Dembski"},
 {id:"ED",title:"Eksploracja danych",color:C_L,day:1,weeks:[2,9,11,13],start:"17:15",end:"19:00",place:"Zajęcia zdalne",teacher:"mgr inż. Dariusz Kobiela"},
   
 {id:"PG",title:"Projekt grupowy II",color:C_P,day:2,weeks:[2,15,16],start:"08:15",end:"10:00",place:"NE AUD1L",teacher:"dr inż. Sławomir Gajewski"},
 {id:"ZBS",title:"Zarządzanie bezpieczeństwem sieci",color:C_W,day:2,weeks:[1,2,3,4,6,8,9],start:"10:15",end:"12:00",place:"NE AUD1P",teacher:"dr inż. Michał Hoeft"},
-{id:"ZFI",title:"Zarządzanie firmą informatyczną",color:C_W,day:2,weeks:[10,11,12,13,14,15,16],start:"10:15",end:"12:00",place:"NE AUD1P",teacher:"dr inż. Paweł Jakóbczyk"},
+{id:"ZFI",title:"Zarządzanie firmą informatyczną",color:C_W,day:2,weeks:[10,11,12,13,14,15],start:"10:15",end:"12:00",place:"NE AUD1P",teacher:"dr inż. Paweł Jakóbczyk"},
 {id:"WZR",title:"Wirtualne zespoły robocze",color:C_W,day:2,weeks:[1,4,6,8,9,10,11,12,13,14,15,16],start:"12:15",end:"13:00",place:"EA AUD1",teacher:"prof. Bogdan Wiszniewski"},
 {id:"SDI",title:"Seminarium dyplomowe inżynierskie I",color:C_S,day:2,weeks:[9,10,11,12,13,14,15,16],start:"16:15",end:"19:00",place:"Zajęcia zdalne",teacher:"dr inż. Jarosław Kuchta"},
 
